e65b8744798556c8b6ba45a8828111b1c64a67d0
RifatHossain41 <rifathossain0423@gmail.com>
2023-06-08 12:50:29 +0600

    added navbar

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div><button className="btn">Button</button>
-    <button className="btn btn-neutral">Neutral</button>
-    <button className="btn btn-primary">Button</button>
-    <button className="btn btn-secondary">Button</button>
-    <button className="btn btn-accent">Button</button>
-    <button className="btn btn-ghost">Button</button>
-    <button className="btn btn-link">Button</button></div>,
+    element: <Main></Main>,
   },
 ]);
 
